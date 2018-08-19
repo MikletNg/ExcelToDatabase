96b9b4de0dfb5c958d631edb3a0ffc4c01b64e46
MikletNg <mikengsinching@icloud.com>
2018-08-19 14:31:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* global config, logger, fs, conn, Sequelize, sequelize */
+/* global config, logger, fs, Sequelize, sequelize */
 global.fs = require("fs");
 global.config = require("./config/config.js");
 
@@ -15,13 +15,13 @@ global.sequelize = new Sequelize(config.mysql.database, config.mysql.user, confi
 });
 
 let hospitals = require("./query/hospitals.js")();
-let surgeries = require("./query/surgeries.js")();
+let operations = require("./query/operations.js")();
 let doctors = require("./query/doctors.js")();
 let hk_table = require("./query/hk_table.js")();
 let sg_table = require("./query/sg_table.js")();
 
 const Hospital = require("./model/hospital.js")();
-const Surgery = require("./model/surgery.js")();
+const Operation = require("./model/operation.js")();
 const Doctor = require("./model/doctor.js")();
 const HKTable = require("./model/hk_table.js")();
 const SGTable = require("./model/sg_table.js")();
@@ -34,8 +34,8 @@ sequelize.sync()
         }
     })
     .then(() => {
-        for (let row in surgeries) {
-            Surgery.create(surgeries[row]);
+        for (let row in operations) {
+            Operation.create(operations[row]);
         }
     })
     .then(() => {
